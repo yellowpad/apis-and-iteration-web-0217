d450635debe091e40c132655a4085d90e938809c
yellowpad <zayneabraham1@gmail.com>
2017-02-08 13:09:58 -0500

    Update command_line_interface.rb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,5 +4,6 @@ end
 
 def get_character_from_user
   puts "please enter a character"
+ character = gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
